e522a6cc308507132a006da8fb43537abbb7a63d
JavaLearnersMore <javalearnersmore@gmail.com>
2026-09-07 00:30:29 +0530

    testing commit

---
+++ src/main/java/com/netbanking/merchant/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.netbanking.merchant.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+	
+	@GetMapping("test")
+	public String testMethod() {
+		
+		return "tesing controller";
+	}
+
+}
